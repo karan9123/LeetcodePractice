cd124c9356e9d16a3a07ab46028ffa2c5a80a826
karan <ka7982@rit.edu>
2023-06-11 22:47:49 +0530

    product of array except self.

---
+++ 238. Product of Array Except Self/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> pre(size+1);
+        vector<int> post(size+1);
+        pre[0] = 1;
+        post[0] = 1;
+
+        for (int i = 0; i < size; i ++){
+            pre[i+1] = pre[i] * nums[i];
+            post[i+1] = post[i] * nums[size-1-i];
+        }
+
+        for (int i = 0; i < size ; i++){
+            pre[i] = pre[i] * post[size-(i+1)];
+        }
+        pre.pop_back();
+        return pre;
+    }
+};
